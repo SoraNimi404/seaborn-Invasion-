29545862c10c2e41452940bf35c4c4797236da6e
ikunofcxk <1079581675@qq.com>
2025-05-16 17:52:37 +0800

    初始化以及模组信息完善

---
+++ src/main/java/com/SoraNimi404/seaborn/SeabornInvasionMod.java
@@ -0,0 +1,10 @@
+package com.SoraNimi404.seaborn;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("seaborn_invasion")  // 与mods.toml中的modId一致
+public class SeabornInvasionMod {
+    public SeabornInvasionMod() {
+        // 注册事件监听或初始化代码
+    }
+}
